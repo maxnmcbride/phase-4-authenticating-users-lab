df5ec4f3307790f5b8a1789f814bfda21be0a218
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2022-12-23 15:55:52 -0500

    commit tree ca5eca299341603bb2ff6ab4e3e0f004c1882213

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    def create
+    #    login creates new session
+        # user's ID = findtheID
+        # render json:
+    end
+
+    def destroy
+    #  logout destroys session info
+        # render json: {errors}
+    end
+
+    private
+
+
+end
